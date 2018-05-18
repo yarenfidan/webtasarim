2a68e4e1a9cbb628cfedd680997fd034142b6698
yarenfidan <yarenfidan10@gmail.com>
2018-05-18 10:54:04 +0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+$('.down').click(function(e){
+ var linkHref = $(this).attr('href');
+ $('html,body').animate({
+ 	scrollTop: $(linkHref).offset().top
+ },1000); 
+});
+
+
+});
